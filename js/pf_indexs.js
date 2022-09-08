f9c1c7ecdae6047e6842e03c131b2bd2009dfa47
Constanza Repetto <repettocj@gmail.com>
2022-09-07 21:58:36 -0300

    cambios 7/09 - 1

---
+++ js/pf_indexs.js
@@ -0,0 +1,58 @@
+
+/*Clase constructora de los distintos platos del menu*/
+class Platos {
+    constructor(nombre, precio, ingredientes, esVegetariano, esVegano) {
+        this.nombre = nombre;
+        this.precio = precio;
+        this.ingredientes = ingredientes;
+        this.esVegetariano = esVegetariano;
+        this.esVegano = esVegano;
+    }
+}
+
+/*Arrays de cada restaurante conteniendo su menu*/
+let raices = []
+let elPreferido = []
+let atilano = []
+
+/*Para agregar los platos*/
+const producto1 = new Platos("Milanesa", 1500, 'ternera, pan rallado, huevo', false, false);
+const producto2 = new Platos("papas fritas", 500, 'papas', true, true);
+const producto3 = new Platos("pure", 500, 'papa, leche, manteca', true, false);
+const producto4 = new Platos("sorrentinos", 1500, 'calabaza, mozarella', true, false);
+
+/*Agrego los platos al restaurante*/
+raices.push(producto1, producto2, producto3,producto4)
+
+/*Funcion para agregar los platos dinamicamente desde la pagina. Pide codigo del plato por parametro*/
+function agregarPlato(){
+    let nombreNuevo = prompt('Ingrese nombre del nuevo Plato');
+    let precioNuevo = parseInt(prompt('Ingrese el precio: $'));
+    let ingredientesNuevo = prompt('Ingrese los Ingredientes:');
+    let vegetarianoNuevo = prompt('Este plato es vegetariano?');
+    let veganoNuevo = prompt('Este platoe s vegano?');
+    nuevoProd = new Platos(nombreNuevo,precioNuevo,ingredientesNuevo,vegetarianoNuevo,veganoNuevo)
+    raices.push(codigo)
+    console.log(raices)
+}
+
+/*Buscar (filtrar) los platos vegetarianos del menu */
+let vegetarianos = raices.filter(platos=>plato.esVegetariano == true)
+console.log(vegetarianos)
+
+
+/*Lista de mesas disponibles */
+let mesa1 = []
+let mesa2 = []
+
+/*Funcion para tomar los pedidos de cada mesa, deberia poder `pasar el numero de mesa por parametro, sino una funcion por cada mesa*/
+function pedidoMesa1(){
+    let productos = []
+    while (productos != 'listo'){
+        productos = productos.push(prompt('Ingresa un plato:'))
+    }
+    mesa1.push(productos)
+}
+
+
+//AFTER 2/09 > 20:12 QUE PONER EN EL PROYECTO FINAL
