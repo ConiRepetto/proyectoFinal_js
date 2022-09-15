f7c051a3be02d02e2772ed2cc4506e2f880a427f
Constanza Repetto <repettocj@gmail.com>
2022-09-15 14:28:16 -0300

    Entrega 15/09 eventos

--- js/pf_index.js
+++ js/pf_index.js
@@ -36,13 +36,15 @@ let madriM = []
 let atilanoM = []
 
 /*Para agregar los platos*/
-const producto1 = new Platos(1, "Milanesa", 'ternera, pan rallado, huevo', false, false, 1500, );
-const producto2 = new Platos(2, "papas fritas", 'papas', true, true, 500);
-const producto3 = new Platos(3, "pure", 'papa, leche, manteca', true, false, 500);
-const producto4 = new Platos(4, "sorrentinos", 'calabaza, mozarella', true, false, 1500);
+const producto1 = new Platos(1, "MILANESA", 'ternera, pan rallado, huevo', false, false, 1500, );
+const producto2 = new Platos(2, "PAPAS FRITAS", 'papas', true, true, 500);
+const producto3 = new Platos(3, "PURE", 'papa, leche, manteca', true, false, 500);
+const producto4 = new Platos(4, "SORRENTINOS", 'calabaza, mozarella', true, false, 1500);
 /*Agrego los platos al menu de un restaurante*/
 raicesM.push(producto1, producto2, producto3, producto4)
 /*Funcion para agregar los platos dinamicamente desde la pagina. Prompt o por formulario*/
+let nuevoProd = {}
+
 function agregarPlato() {
     let numeroNuevo = prompt('Ingrese numero del nuevo Plato');
     let nombreNuevo = prompt('Ingrese nombre del nuevo Plato');
@@ -58,38 +60,48 @@ function agregarPlato() {
 /*Para agregar platos al menu por formulario (inputs)*/
 function agregar() {
     const inputNumero = document.querySelector('#inputNumero')
+    if (inputNumero == '') {
+        alert('Ingrese de nuevo:')
+    } else if (raicesM.filter(Platos => Platos.numero == inputNumero)) {
+        alert('Nuemro ya ingresado, agregue numero nuevo:')
+    } else if (inputNumero == NaN) {
+        alert('Valor ingresado invalido, ingrese un numero:')
+    }
     const inputNombre = document.querySelector('#inputNombre')
+    if (inputNombre == '') {
+        alert('Ingrese de nuevo:')
+    } else if (raicesM.filter(Platos => Platos.nombre == inputNombre)) {
+        alert('Nombre ya ingresado, agregue nombre nuevo:')
+    }
     const inputPrecio = document.querySelector('#inputPrecio')
+    if (inputPrecio >= 3000) {
+        alert('Precio demasiado alto:')
+    }
     const inputIngredientes = document.querySelector('#inputIngredientes')
+    if (inputNumero == '') {
+        alert('Ingrese de nuevo:')
+    }
     const inputVegetariano = document.querySelector('#inputVegetariano')
     const inputVegano = document.querySelector('#inputVegano')
 
-    const btnAgregar = document.querySelector('.btnAgregar')
+    const btnAgregar = document.querySelector('#button-addon2')
     btnAgregar.addEventListener('click', () => {
         nuevoProd = new Platos(inputNumero, inputNombre, inputIngredientes, inputVegetariano, inputVegano, inputPrecio)
         raicesM.push(nuevoProd)
         console.table(raicesM)
         alert('Plato Agregado al Menu')
     })
-    
-}
-
-
-/*Buscar (filtrar) los platos vegetarianos del menu */
-function verVegetarianos() {
-    let vegetarianos = raices.filter(Platos => Platos.esVegetariano == true)
-    console.table(vegetarianos)
 }
 
 /*array de todos los inputs dentro de mi document, itero sobre todos y deveulvo el valor de cada uno (despues lo capturo en una variable nueva)*/
-const inputs = document.querySelectorAll('input')
-inputs.forEach(input => {
-    console.log(input.value)
-    input.addEventListener('input', () => console.log(input.value))
-})
+// const inputs = document.querySelectorAll('input')
+// inputs.forEach(input => {
+//     console.log(input.value)
+//     input.addEventListener('input', () => console.log(input.value))
+// })
 
 /*Llenar la tabla del menu con todos los platos (plantilla > tamplate strings) LO HACE SOLO, LE SAQUE LA FUNCION*/
-const tablaB = document.querySelector('#tablaB')
+// const tablaB = document.querySelector('#tablaB')
 
 let fila = ''
 
@@ -124,26 +136,50 @@ function agregarFilaHTML() {
 
 }
 
-const inputBuscarPlato = document.querySelector("#btnBuscarPlato")
 
+//FILTRAR PRODUCTOS EN LA TABLA INGRESANDO PARTE DEL NOMBRE
 
-function filtrarPlatos() { //FILTRAR PRODUCTOS EN LA TABLA INGRESANDO PARTE DEL NOMBRE
+const inputBuscarPlato = document.querySelector("#inputBuscarPlato")
+
+function filtrarPlatos() {
     inputBuscarPlato.value = inputBuscarPlato.value.trim().toUpperCase()
     if (inputBuscarPlato.value !== "") {
         const resultado = raicesM.filter(Platos => Platos.nombre.includes(inputBuscarPlato.value))
+        agregarFilaBuscado(resultado)
+        console.table(resultado)
         if (resultado.length === 0) {
             console.clear()
             console.warn("No se encontraron platos.")
-            agregarFilaHTML(raicesM)
-        } else {
-            agregarFilaHTML(resultado)
         }
     } else {
-        agregarFilaHTML(raicesM)
+        console.clear()
+        console.warn("No se encontraron platos.")
     }
 }
+const btnBuscarPlato = document.querySelector('#btnBuscarPlato')
+btnBuscarPlato.addEventListener('click', filtrarPlatos)
 
-inputBuscarPlato.addEventListener("#btnBuscarPlato", filtrarPlatos) //A medida que escribimos.
+function agregarFilaBuscado(platoBuscado) {
+    let fila = ''
+    
+    raicesM[platoBuscado] 
+        fila = `<tr>                          
+                        <th scope="row">${platoBuscado.numero}</th>
+                        <td class="nombre">${platoBuscado.nombre}</td>
+                        <td class="ingredientes">${platoBuscado.ingredientes}</td>
+                        <td class="vegetariano">${platoBuscado.vegetariano}</td>
+                        <td class="vegano">${platoBuscado.vegano}</td>
+                        <td class="precio">${platoBuscado.precio}</td>
+                        <td><button id="btn${platoBuscado.nombre}">+</button></td>
+                    </tr>`;
+        tablaB.innerHTML += fila
+}
+
+/*Buscar (filtrar) los platos vegetarianos del menu */
+function verVegetarianos() {
+    let vegetarianos = raicesM.filter(Platos => Platos.esVegetariano == true)
+    console.table(vegetarianos)
+}
 
 
 /* PEDIDO */
@@ -164,6 +200,6 @@ function agregarAlPedido(nombreSelec) {
     const plato = raicesM.find(Platos => Platos.nombre == nombreSelec)
     mesa1.push(plato)
     console.table(mesa1)
-    alert('LA PUTA MADRE')
+    alert('xxxxx')
     //localStorage.setItem("mesa1", JSON.stringify(mesa1))
 }
